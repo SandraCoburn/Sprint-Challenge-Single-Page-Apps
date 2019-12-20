dcf031b671a900f4d034ff8e34a59af0f8d0f0d4
Sandra Coburn <sandra_coburn@hotmail.com>
2019-12-20 12:27:08 -0800

    Added Lcation card and location list as a stretch goal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
-import SearchForm from "./components/SearchForm";
+import LocationList from "./components/LocationsList";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 export default function App() {
@@ -13,11 +13,15 @@ export default function App() {
         <div className="nav-links">
           <Link to="/">Welcome Page</Link>
           <Link to="/CharacterList">Character List</Link>
+          <Link to="/LocationList">Location List</Link>
         </div>
       </nav>
       <Switch>
         <Route path="/CharacterList">
           <CharacterList />
+        </Route>
+        <Route path="/LocationList">
+          <LocationList />
         </Route>
         <Route path="/">
           <WelcomePage />
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { Card, CardBody, Col, CardHeader, Container } from "reactstrap";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+const MyCardColors = styled.div`
+  color: brown;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+`;
+
+const MyCardTitle = styled.div`
+  color: blue;
+  font-size: 1.5rem;
+`;
+
+export default function LocationCard(props) {
+  return (
+    <div>
+      <Container>
+        <div className="card-list">
+          {/* <Col xs="6" md="4" xl="3"> */}
+          <Card>
+            <CardHeader> Name: </CardHeader>
+            <MyCardTitle>{props.character.name} </MyCardTitle>
+            <CardBody>
+              <MyCardColors>Type: {props.character.type}</MyCardColors>
+              <MyCardColors>
+                Dimension: {props.character.dimension}
+              </MyCardColors>
+              <MyCardColors>
+                Residents: {props.character.residents}
+              </MyCardColors>
+            </CardBody>
+          </Card>
+          {/* </Col> */}
+        </div>
+      </Container>
+    </div>
+  );
 }
