a36481ac1b7d989d5df339f4df1ffe305070b460
Sandra Coburn <sandra_coburn@hotmail.com>
2019-12-20 11:45:44 -0800

    Made search form functional. Answered self reflect question.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const handleInputChange = props.handleInputChange;
+  const query = props.query;
   return (
     <section className="search-form">
       <form className="search">
+        <label htmlFor="name">Search:</label>
         <input
           type="text"
-          // onChange={handleInputChange}
-          // value={query}
+          onChange={handleInputChange}
+          value={query}
           name="name"
           tabIndex="0"
           className="prompt search-name"
