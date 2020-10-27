735249b0e674766e203b5b787dd0508ac332bdd1
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-26 20:20:57 -0700

    Saved changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 import LocationList from "./components/LocationsList";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
     <main>
       <Header />
       <nav>
-        <div className="nav-links">
-          <Link to="/">Welcome Page</Link>
-          <Link to="/CharacterList">Character List</Link>
-          <Link to="/LocationList">Location List</Link>
+        <div className="item-sub-nav">
+          <NavLink to="/">Welcome Page</NavLink>
+          <NavLink to="/CharacterList">Character List</NavLink>
+          <NavLink to="/LocationList">Location List</NavLink>
         </div>
       </nav>
       <Switch>
